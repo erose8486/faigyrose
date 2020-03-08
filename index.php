a9de5a07a830ebe0174e542d659ac30a17117f05
Esty <erosenberg8486@gmail.com>
2020-03-08 14:03:24 -0400

    current live site

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="norton-safeweb-site-verification" content="md80ih7-qervv4douhl76x3dk3i7938-bdkgl6mkek27xk40xdcr3acz2hpxqszyj-l929jmc1z9mdooaomv7a97r17465waa34330kswavxe16ljjq93z5ptc1fdy8s" />
+  <title>FaigyRose Photography</title>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+  <meta name="google-site-verification" content="6otjz7ce7C62BOUdWf7skOAnHuOaji0XlyeSMLSF7-4" />
+<meta name="norton-safeweb-site-verification" content="md80ih7-qervv4douhl76x3dk3i7938-bdkgl6mkek27xk40xdcr3acz2hpxqszyj-l929jmc1z9mdooaomv7a97r17465waa34330kswavxe16ljjq93z5ptc1fdy8s" />
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.5.0/knockout-min.js"></script>
+  <script src="https://kit.fontawesome.com/a2030581f8.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="styles.css">
+  <script src="script.js" defer></script>
+</head>
+<body>
+
+  <nav id="main_nav">
+    <div class="text-center"><img src="logo.png" width="200px" ></div>
+    <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+      <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab">Home</a>
+      <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="true">About</a>
+      <a class="nav-item nav-link" id="nav-portfolio-tab" data-toggle="tab" href="#nav-portfolio" role="tab" aria-controls="nav-portfolio" aria-selected="false">Portfolio</a>
+      <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+    </div>
+  </nav>
+  <div class="tab-content">
+  <div id="nav-home" class="tab-pane active clearfix row" role="tabpanel">
+    <div class="col-xs-12 col-md-6 float-left">
+      <div class="text-center" id="home-header">
+      <h1>Still Life Photography <i class="fas fa-camera"></i></h1>
+      <h2>Make a statement without saying a word</h2>
+      </div>
+      <div id="main-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#main-carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#main-carousel" data-slide-to="1"></li>
+          <li data-target="#main-carousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="camera.jpg" class="d-block w-100">
+            <div class="carousel-caption d-none d-lg-block">
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="l17.jpg" class="d-block w-100">
+          </div>
+          <div class="carousel-item">
+            <img src="skyline5.jpg" class="d-block w-100">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-12 col-md-5 float-right text-center">
+      <a href="https://www.instagram.com/faigyrosephotography/"><img src="insta.jpg" style="width:35%"></a>
+      <div style="height:800px;overflow:auto">
+      <div class='embedsocial-instagram' data-ref="de74584717c54c0ee29e2a8f0f8ae6150c46b84f"></div><script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/embedscript/in.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialInstagramScript"));</script>
+      </div>
+    </div>
+  </div>
+  <div id="nav-about" class="tab-pane text-center" role="tabpanel">
+    <h1>HI!</h1>
+    <h3>My name is Faigy Rosenberg, I’m a still-life photographer from Brooklyn.</h3>
+    <p>My passion for photography started at a young age when I received my first disposable camera at age 9.<br>
+       Since then I have been photographing everything in sight.<br>
+       I took my photography to the next level when I joined NYIP and graduated in 2016.<br>
+       When I discovered still-life photography, I was hooked.<br>
+       What I love most about still-life is the way I can create a story with one image.<br>
+       <em style="font-weight:bold">Whether it’s through props or lighting, each image is a new challenge and one that I fully embrace in order to ensure that you receive the best possible image of your product.</em></p>
+  </div>
+  <div id="nav-portfolio" class="tab-pane" role="tabpanel">
+    <div id="options" class="row">
+      <div class="custom-card col-sm-4" id="productBtn" data-bind="click:viewProducts">
+        <img src="p2.jpeg">
+        <p>PRODUCTS</p>
+      </div>
+      <div class="custom-card col-sm-4" id="lifestyleBtn" data-bind="click:viewLifestyle">
+        <img src="l11.jpeg">
+        <p>LIFESTYLE</p>
+      </div>
+      <div class="custom-card col-sm-4" id="allBtn" data-bind="click:viewAll">
+        <img src="camera.jpg">
+        <p>ALL</p>
+      </div>
+    </div>
+    <div data-bind="foreach:visible" class="row">
+      <div class="col-md-3 col-sm-2 photos" data-bind="click:$root.single">
+        <img data-bind="attr:{src:$data}" class="images" data-toggle="modal" data-target="#singleImg">
+        <i class="fas fa-search-plus zoom" data-bind="click:$root.single"></i>
+      </div>
+    </div>
+  </div>
+  <div id="nav-contact" class="tab-pane text-center" role="tabpanel">
+  <?php
+$action=$_REQUEST['action'];
+if ($action=="")    /* display the contact form */
+    {
+    ?>
+    <form  action="" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="action" value="submit">
+    Your name:<br>
+    <input name="name" type="text" value="" size="30"/><br>
+    Your email:<br>
+    <input name="email" type="text" value="" size="30"/><br>
+    Your message:<br>
+    <textarea name="message" rows="7" cols="30"></textarea><br>
+    <input type="submit" value="Send email"/>
+    </form>
+    <?php
+    } 
+else                /* send the submitted data */
+    {
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{        
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+        mail("erosenberg8486@gmail.com", $subject, $message, $from);
+        echo "Email sent!";
+        }
+    }  
+?>
+    <!--<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfuEJJVy4TvgpSpcP_zBreFT-V1drjpo_TrL6X2TnkUP3CyDQ/viewform?embedded=true" style="width:75vw" height='750' frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>  </div>-->
+  </div>
+  <footer id="footer">
+    <div id="contactInfo">
+    <span><a href="https://www.instagram.com/faigyrosephotography/"><i class="fab fa-instagram"></i> Faigyrosephotography</a></span>
+    <span><a href="mailto:faigyrosephotography@gmail.com"><i class="fas fa-at"></i> faigyrosephotography@gmail.com</a></span>
+    <span><a href="tel:17189284043"><i class="fas fa-phone-alt"></i> 718-928-4043</a></span>
+    </div>
+    <p class="text-center" style="padding:5px"><small>© 2020 EstyRose</small></p>
+  </footer>
+  <div class="modal" id="singleImg">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+      <div class="modal-body">
+      <button data-bind="click:prev" id="prevArrow" class="btn btn-outline-light btn-lg"><i class="fas fa-arrow-left"></i></button>
+      <img data-bind="attr:{src:chosen}" width="100%">
+      <button data-bind="click:next" id="nextArrow" class="btn btn-outline-light btn-lg"><i class="fas fa-arrow-right"></i></button>
+      </div></div>
+    </div>
+  </div>
+</body>
+</html>
